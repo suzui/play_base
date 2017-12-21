9cdb172fc0df46a01326e2a20ebf6dc68165b164
suzui <yechenfan@vip.163.com>
2017-12-21 10:25:38 +0800

    m

---
+++ app/utils/TextUtils.java
@@ -0,0 +1,28 @@
+package utils;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextUtils {
+    public static void main(String[] args) {
+        File file = new File("log");
+        LineIterator it = null;
+        try {
+            it = FileUtils.lineIterator(file, "UTF-8");
+            while (it.hasNext()) {
+                String s = it.next();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            LineIterator.closeQuietly(it);
+        }
+        
+    }
+}
+
