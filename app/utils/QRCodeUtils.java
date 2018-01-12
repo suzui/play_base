3f36d88774321f7b68ebf777ec109bcd6c3f7d82
suzui <yechenfan@vip.163.com>
2018-01-12 18:27:25 +0800

    m

--- app/utils/QRCodeUtils.java
+++ app/utils/QRCodeUtils.java
@@ -8,6 +8,8 @@ import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import org.apache.commons.lang.RandomStringUtils;
+import play.Play;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -18,49 +20,64 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class QRCodeUtils {
     
-    public static void gen(String filePath, String logoPath, String content) throws WriterException, IOException {
-        int width = 200; // 图像宽度
-        int height = 200; // 图像高度
-        String format = "png";// 图像类型
-        Map<EncodeHintType, Object> hints = new HashMap<EncodeHintType, Object>();
-        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");//编码
-        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);//容错
-        BitMatrix bitMatrix = new MultiFormatWriter().encode(content,
-                BarcodeFormat.QR_CODE, width, height, hints);// 生成矩阵
-        Path path = FileSystems.getDefault().getPath(filePath);
-        MatrixToImageConfig config = new MatrixToImageConfig();
-        MatrixToImageWriter.writeToPath(bitMatrix, format, path, config);// 输出图像
-        
-        BufferedImage image = ImageIO.read(new File(filePath));
-        Graphics2D g = image.createGraphics();
-        //logo起始位置，此目的是为logo居中显示
-        int w = width / 5;
-        int h = height / 5;
-        int x = (width - w) / 2;
-        int y = (height - h) / 2;
-        //绘制图
-        BufferedImage logo = ImageIO.read(new File(logoPath));
-        g.drawImage(logo, x, y, w, h, null);
-        
-        //给logo画边框
-        //构造一个具有指定线条宽度以及 cap 和 join 风格的默认值的实心 BasicStroke
-        //g.setStroke(new BasicStroke(2));
-        //g.setColor(Color.RED);
-        //g.drawRect(x, y, width, height);
-        
-        g.dispose();
-        //写入logo照片到二维码
-        ImageIO.write(image, format, new File(filePath));
+    public static void gen(String filePath, String logoPath, String content) {
+        try {
+            
+            
+            int width = 200; // 图像宽度
+            int height = 200; // 图像高度
+            String format = "jpg";// 图像类型
+            Map<EncodeHintType, Object> hints = new HashMap<EncodeHintType, Object>();
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");//编码
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);//容错
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(content,
+                    BarcodeFormat.QR_CODE, width, height, hints);// 生成矩阵
+            Path path = FileSystems.getDefault().getPath(filePath);
+            MatrixToImageConfig config = new MatrixToImageConfig();
+            MatrixToImageWriter.writeToPath(bitMatrix, format, path, config);// 输出图像
+            
+            BufferedImage image = ImageIO.read(new File(filePath));
+            Graphics2D g = image.createGraphics();
+            //logo起始位置，此目的是为logo居中显示
+            int w = width / 4;
+            int h = height / 4;
+            int x = (width - w) / 2;
+            int y = (height - h) / 2;
+            //绘制图
+            BufferedImage logo = ImageIO.read(new File(logoPath));
+            //g.drawImage(logo, x, y, w, h, null);
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g.drawImage(
+                    logo.getScaledInstance(w, h, Image.SCALE_SMOOTH),
+                    x, y, null);
+            
+            //给logo画边框
+            //构造一个具有指定线条宽度以及 cap 和 join 风格的默认值的实心 BasicStroke
+            g.setStroke(new BasicStroke(1));
+            g.setColor(Color.RED);
+            g.drawRect(x, y, w, h);
+            
+            g.dispose();
+            //写入logo照片到二维码
+            ImageIO.write(image, format, new File(filePath));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
-    public static void main(String[] args) throws Exception {
-        String filePath = "/Users/suzui/Desktop/1.png";
-        String logoPath = "";
-        String content = "";
+    public static File gen(String content) {
+        String filePath = "tmp/" + RandomStringUtils.randomNumeric(16) + ".jpg";
+        String logoPath = "public/images/logo/logo50_50.jpg";
         gen(filePath, logoPath, content);
+        return new File(filePath);
+    }
+    
+    public static void main(String[] args) {
+        gen("xxx");
         return;
     }
     
--- app/utils/QiniuUtils.java
+++ app/utils/QiniuUtils.java
@@ -11,57 +11,51 @@ import com.qiniu.util.Auth;
 import play.Play;
 import play.cache.Cache;
 
+import java.io.File;
+
 public class QiniuUtils {
     
     public final static String UPTOKENKEY = "qiniuUptoken";
+    public final static String ACCESSKEY = Play.configuration.getProperty("qiniu.accessKey");
+    public final static String SECRETKEY = Play.configuration.getProperty("qiniu.secretKey");
+    public final static String BUCKET = Play.configuration.getProperty("qiniu.bucket");
+    public final static String DOMAIN = Play.configuration.getProperty("qiniu.domain");
     
     public static String upToken() {
         Object uptoken = Cache.get(UPTOKENKEY);
         if (uptoken != null) {
             return (String) uptoken;
         }
-        String accessKey = Play.configuration.getProperty("qiniu.accessKey");
-        String secretKey = Play.configuration.getProperty("qiniu.secretKey");
-        String bucket = Play.configuration.getProperty("qiniu.bucket");
-        Auth auth = Auth.create(accessKey, secretKey);
-        String upToken = auth.uploadToken(bucket, null, 86400, null);
+        Auth auth = Auth.create(ACCESSKEY, SECRETKEY);
+        String upToken = auth.uploadToken(BUCKET, null, 86400, null);
         Cache.set(UPTOKENKEY, upToken);
         return upToken;
     }
     
-    public static void upload() {
+    public static String upload(File file) {
         //构造一个带指定Zone对象的配置类
         Configuration cfg = new Configuration(Zone.zone0());
         //...其他参数参考类注释
         UploadManager uploadManager = new UploadManager(cfg);
         //...生成上传凭证，然后准备上传
-        String accessKey = "your access key";
-        String secretKey = "your secret key";
-        String bucket = "your bucket name";
-        //如果是Windows情况下，格式是 D:\\qiniu\\test.png
-        String localFilePath = "/home/qiniu/test.png";
         //默认不指定key的情况下，以文件内容的hash值作为文件名
-        String key = null;
-        Auth auth = Auth.create(accessKey, secretKey);
-        String upToken = auth.uploadToken(bucket);
+        String key = file.getName();
+        String upToken = upToken();
         try {
-            Response response = uploadManager.put(localFilePath, key, upToken);
+            Response response = uploadManager.put(file.getAbsolutePath(), key, upToken);
             //解析上传成功的结果
             DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
-            System.out.println(putRet.key);
-            System.out.println(putRet.hash);
+            //System.out.println(putRet.key);
+            //System.out.println(putRet.hash);
+            return DOMAIN + "/" + putRet.key;
         } catch (QiniuException ex) {
             Response r = ex.response;
-            System.err.println(r.toString());
-            try {
-                System.err.println(r.bodyString());
-            } catch (QiniuException ex2) {
-                //ignore
-            }
+            //System.err.println(r.toString());
+            return null;
         }
     }
     
     public static void main(String[] args) {
-    
+        upload(QRCodeUtils.gen("xxx"));
     }
 }
