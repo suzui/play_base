5afa3c29e7ef053d49abcac1d62ad1b3c87452aa
suzui <yechenfan@vip.163.com>
2018-04-14 19:05:48 +0800

    m

--- app/listeners/SSOModelListener.java
+++ app/listeners/SSOModelListener.java
@@ -4,6 +4,7 @@ import models.sso.SsoModel;
 import models.sso.SsoOrganize;
 import models.sso.SsoPerson;
 import models.sso.SsoRelation;
+import play.Play;
 import results.sso.OrganizeResult;
 import results.sso.PersonResult;
 import results.sso.RelationResult;
@@ -16,40 +17,45 @@ public class SSOModelListener {
     
     @PrePersist
     public static void prePersist(SsoModel ssoModel) {
-        if (ssoModel instanceof SsoOrganize) {
-            OrganizeResult organizeResult = SSOUtils.organizeAdd((SsoOrganize) ssoModel);
-            ssoModel.preUpdate(organizeResult.data.organizeId);
-        } else if (ssoModel instanceof SsoPerson) {
-            PersonResult personResult = SSOUtils.personAdd((SsoPerson) ssoModel);
-            ssoModel.preUpdate(personResult.data.personId);
-        } else if (ssoModel instanceof SsoRelation) {
-            RelationResult relationResult = SSOUtils.relationAdd((SsoRelation) ssoModel);
-            ssoModel.preUpdate(relationResult.data.relationId);
+        if (Play.configuration.getProperty("sso", "off").equals("on")) {
+            if (ssoModel instanceof SsoOrganize) {
+                OrganizeResult organizeResult = SSOUtils.organizeAdd((SsoOrganize) ssoModel);
+                ssoModel.preUpdate(organizeResult.data.organizeId);
+            } else if (ssoModel instanceof SsoPerson) {
+                PersonResult personResult = SSOUtils.personAdd((SsoPerson) ssoModel);
+                ssoModel.preUpdate(personResult.data.personId);
+            } else if (ssoModel instanceof SsoRelation) {
+                RelationResult relationResult = SSOUtils.relationAdd((SsoRelation) ssoModel);
+                ssoModel.preUpdate(relationResult.data.relationId);
+            }
         }
+        
     }
     
     @PreUpdate
     public static void preUpdate(SsoModel ssoModel) {
-        if (ssoModel instanceof SsoOrganize) {
-            SsoOrganize ssoOrganize = (SsoOrganize) ssoModel;
-            if (ssoOrganize.deleted) {
-                SSOUtils.organizeDelete(ssoOrganize);
-            } else {
-                SSOUtils.organizeEdit(ssoOrganize);
-            }
-        } else if (ssoModel instanceof SsoPerson) {
-            SsoPerson ssoPerson = (SsoPerson) ssoModel;
-            if (ssoPerson.deleted) {
-                SSOUtils.personDelete(ssoPerson);
-            } else {
-                SSOUtils.personEdit(ssoPerson);
-            }
-        } else if (ssoModel instanceof SsoRelation) {
-            SsoRelation ssoRelation = (SsoRelation) ssoModel;
-            if (ssoRelation.deleted) {
-                SSOUtils.relationDelete(ssoRelation);
-            } else {
-                SSOUtils.relationEdit(ssoRelation);
+        if (Play.configuration.getProperty("sso", "off").equals("on")) {
+            if (ssoModel instanceof SsoOrganize) {
+                SsoOrganize ssoOrganize = (SsoOrganize) ssoModel;
+                if (ssoOrganize.deleted) {
+                    SSOUtils.organizeDelete(ssoOrganize);
+                } else {
+                    SSOUtils.organizeEdit(ssoOrganize);
+                }
+            } else if (ssoModel instanceof SsoPerson) {
+                SsoPerson ssoPerson = (SsoPerson) ssoModel;
+                if (ssoPerson.deleted) {
+                    SSOUtils.personDelete(ssoPerson);
+                } else {
+                    SSOUtils.personEdit(ssoPerson);
+                }
+            } else if (ssoModel instanceof SsoRelation) {
+                SsoRelation ssoRelation = (SsoRelation) ssoModel;
+                if (ssoRelation.deleted) {
+                    SSOUtils.relationDelete(ssoRelation);
+                } else {
+                    SSOUtils.relationEdit(ssoRelation);
+                }
             }
         }
     }
