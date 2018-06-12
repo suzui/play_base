c8bb9408f6cd8d2b0393e68fd3acb5f95be7a6c4
suzui <yechenfan@vip.163.com>
2018-06-12 15:49:58 +0800

    m

--- app/utils/BaseUtils.java
+++ app/utils/BaseUtils.java
@@ -1,11 +1,34 @@
 package utils;
 
 import play.Play;
+import play.jobs.Job;
+import play.jobs.JobsPlugin;
+import play.utils.Java;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ScheduledFuture;
 
 public class BaseUtils {
     
     public static boolean isProd() {
         return "p".equals(Play.id);
+    }
+    
+    public static void cancelJob(Class clazz) {
+        BlockingQueue<Runnable> queue = JobsPlugin.executor.getQueue();
+        for (final Object o : queue) {
+            ScheduledFuture task = (ScheduledFuture) o;
+            if (task.isDone() || task.isCancelled()) {
+                continue;
+            }
+            Job job = (Job) Java.extractUnderlyingCallable((FutureTask) task);
+            if (clazz.isAssignableFrom(job.getClass())) {
+                task.cancel(true);
+                break;
+            }
+        }
+        
     }
     
 }
