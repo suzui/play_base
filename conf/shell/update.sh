3425b9fe4ce46f8e9ff5aea40af5f43097f2da86
suzui <yechenfan@vip.163.com>
2018-07-23 16:39:26 +0800

    m

--- conf/shell/update.sh
+++ conf/shell/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd $3;
 ls;
-echo $2 | sudo -u $1 -S git checkout $4;
-sudo -u $1 git pull << EOF
+#echo $2 | sudo -u $1 -S git checkout $4;
+sudo -u $1 -S git pull << EOF
 $2
 EOF
