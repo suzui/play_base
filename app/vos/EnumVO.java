26627428c529c4dce9a817b6796a8f20b090e9c5
suzui <yechenfan@vip.163.com>
2018-08-20 22:17:22 +0800

    enums

--- app/vos/EnumVO.java
+++ app/vos/EnumVO.java
@@ -2,8 +2,13 @@ package vos;
 
 import annotations.DataField;
 import interfaces.BaseEnum;
+import utils.BaseUtils;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class EnumVO extends OneData implements Serializable {
     
@@ -15,6 +20,20 @@ public class EnumVO extends OneData implements Serializable {
     public EnumVO(BaseEnum baseEnum) {
         this.code = baseEnum.code();
         this.intro = baseEnum.intro();
+    }
+    
+    public static List<EnumVO> list(List<BaseEnum> enums) {
+        if (BaseUtils.collectionEmpty(enums)) {
+            return Collections.EMPTY_LIST;
+        }
+        return enums.stream().map(e -> new EnumVO(e)).collect(Collectors.toList());
+    }
+    
+    public static List<EnumVO> list(BaseEnum[] enums) {
+        if (enums == null || enums.length == 0) {
+            return Collections.EMPTY_LIST;
+        }
+        return list(Arrays.asList(enums));
     }
     
 }
