c90244906ea493003809c64bc49c26670726a9c0
suzui <yechenfan@vip.163.com>
2018-09-19 10:31:26 +0800

    m

--- app/jobs/ProStopJob.java
+++ app/jobs/ProStopJob.java
@@ -12,7 +12,7 @@ public class ProStopJob extends Job {
     @Override
     public void doJob() throws Exception {
         Pro pro = Pro.findByLocation(Play.applicationPath.getAbsolutePath());
-        if (pro != null) {
+        if (pro != null && pro.status != ProStatus.START) {
             pro.status(ProStatus.STOP);
         }
     }
