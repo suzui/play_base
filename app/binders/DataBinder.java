2cca7308f5886cfaa3c95810a273fe2126782ca0
suzui <yechenfan@vip.163.com>
2018-10-09 16:41:22 +0800

    m

--- app/binders/DataBinder.java
+++ app/binders/DataBinder.java
@@ -11,7 +11,6 @@ import utils.BaseUtils;
 import vos.OneData;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
@@ -27,30 +26,32 @@ public class DataBinder implements TypeBinder<OneData> {
         Map<String, Object> params = new HashMap<>();
         String lang = BaseUtils.propertyOn("i18n") ? Lang.get() : null;
         for (Entry<String, String> e : Request.current().params.allSimple().entrySet()) {
-            String k = e.getKey(), v = e.getValue();
-            if (k.equals("vo") || k.equals("body")) continue;//vo body 参数过滤
-            if (v == null || v.equals("null") || StringUtils.equals(value, "(null)") || v.equals("undefined") || v.equals("NaN")) continue;//空值过滤
-            if (v.equals("")) {
-                Field field = actualClass.getField(k);
-                if (field == null) continue;
-                if (!String.class.isAssignableFrom(field.getType())) continue;//非string类型空值过滤
-            } else if (v.startsWith("[") && v.endsWith("]")) {
-                Field field = actualClass.getField(k);
-                if (field == null) continue;
-                if (List.class.isAssignableFrom(field.getType())) params.put(k, GSON.fromJson(v, List.class));//list类型处理
-                else params.put(k, v);
-            } else if (v.startsWith("{") && v.endsWith("}")) {
-                Field field = actualClass.getField(k);
-                if (field == null) continue;
-                if (OneData.class.isAssignableFrom(field.getType())) params.put(k, GSON.fromJson(v, Map.class));//vo类型处理
-                else params.put(k, v);
-            } else {
-                if (StringUtils.isNotBlank(lang)) {
-                    char[] cs = k.toCharArray();
-                    cs[0] -= 32;
-                    params.put(lang + String.valueOf(cs), v);
+            try {
+                String k = e.getKey(), v = e.getValue();
+                if (k.equals("vo") || k.equals("body")) continue;//vo body 参数过滤
+                if (v == null || v.equals("null") || StringUtils.equals(value, "(null)") || v.equals("undefined") || v.equals("NaN"))
+                    continue;//空值过滤
+                if (v.equals("")) {
+                    if (!String.class.isAssignableFrom(actualClass.getField(k).getType())) continue;//非string类型空值过滤
+                    else params.put(k, v);
+                } else if (v.startsWith("[") && v.endsWith("]")) {
+                    if (!String.class.isAssignableFrom(actualClass.getField(k).getType()))
+                        params.put(k, GSON.fromJson(v, List.class));//list类型处理
+                    else params.put(k, v);
+                } else if (v.startsWith("{") && v.endsWith("}")) {
+                    if (!String.class.isAssignableFrom(actualClass.getField(k).getType()))
+                        params.put(k, GSON.fromJson(v, Map.class));//vo类型处理
+                    else params.put(k, v);
+                } else {
+                    if (StringUtils.isNotBlank(lang)) {
+                        char[] cs = k.toCharArray();
+                        cs[0] -= 32;
+                        params.put(lang + String.valueOf(cs), v);
+                    }
+                    params.put(k, v);
                 }
-                params.put(k, v);
+            } catch (Exception exception) {
+                continue;
             }
         }
         if (!params.containsKey("page")) params.put("page", 1);
