fcb6c7edf24d9d1d5a4aaa5635cf86664d0bec44
suzui <yechenfan@vip.163.com>
2018-10-25 14:37:08 +0800

    layui

--- public/back/js/back.js
+++ public/back/js/back.js
@@ -139,11 +139,10 @@ $.ajaxSetup({
     async: false
 });
 
-
 // 对Date的扩展，将 Date 转化为指定格式的String
 // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
 // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
-Date.prototype.Format = function (fmt) { //author: meizz
+Date.prototype.Format = function (fmt) {
     var o = {
         "M+": this.getMonth() + 1, //月份
         "d+": this.getDate(), //日
