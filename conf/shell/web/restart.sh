224563cb2111dfcc0a6dd5ec4c85a6794bbc7c45
suzui <yechenfan@vip.163.com>
2019-01-09 17:27:12 +0800

    m

--- conf/shell/web/restart.sh
+++ conf/shell/web/restart.sh
@@ -3,4 +3,5 @@ cd $3;
 ls;
 echo '$2' |su $1;
 lsof -i:$4 |awk 'NR==2{print $2}' |xargs kill -9;
+npm install;
 npm run build;
--- conf/shell/web/start.sh
+++ conf/shell/web/start.sh
@@ -2,4 +2,5 @@
 cd $3;
 ls;
 echo '$2' |su $1;
+npm install;
 npm run build;
